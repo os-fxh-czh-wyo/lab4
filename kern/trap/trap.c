0af0b5ac0c61e6fa175300898ba2d2d70aea0381
Yanou Wang <2082573831@qq.com>
2025-11-12 18:47:15 +0800

    trap.c

--- kern/trap/trap.c
+++ kern/trap/trap.c
@@ -111,6 +111,17 @@ void interrupt_handler(struct trapframe *tf)
         // clear_csr(sip, SIP_STIP);
 
         /*LAB3 请补充你在lab3中的代码 */ 
+        clock_set_next_event();
+        ticks++;
+        static int num=0;
+        if(ticks==TICK_NUM){
+            print_ticks();
+            num++;
+            ticks=0;
+        }
+        if(num==10){
+            sbi_shutdown();
+        }
         break;
     case IRQ_H_TIMER:
         cprintf("Hypervisor software interrupt\n");
